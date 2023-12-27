39ae160bb3fa6bf4dbd4247a3d773219ba0acce7
Prince527 <Prince527gaming@gmail.com>
2023-12-26 22:25:26 -0400

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -9,44 +9,14 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, message, args) => {
+        const circles = { green: "🟢", yellow: "🟡", red: "🔴" };
+        
         const now = Date.now();
 
         const msg = await message.channel.sendMessage("Pinging...");
 
-        msg.edit({ content: `The bots latency is: ${Date.now() - now}ms` });
+        const botLatency = Date.now() - now;
 
-        // const circles = { green: "🟢", yellow: "🟡", red: "🔴" };
-
-        // const days = Math.floor(client.uptime / 86400000);
-        // const hours = Math.floor(client.uptime / 3600000) % 24;
-        // const minutes = Math.floor(client.uptime / 60000) % 60;
-        // const seconds = Math.floor(client.uptime / 1000) % 60;
-
-        // const botLatency = new Date() - message.createdAt
-        // const apiLatency = client.ws.ping;
-
-        // message.channel.send({
-        //     embeds: [
-        //         new EmbedBuilder()
-        //         .setColor("Random")
-        //         .addFields([
-        //             {
-        //                 name: "Bot Latency",
-        //                 value: `${botLatency <= 200 ? circles.green : botLatency <= 400 ? circles.yellow : circles.red} ${botLatency}ms`,
-        //                 inline: true
-        //             },
-        //             {
-        //                 name: "API Latency",
-        //                 value: `${apiLatency <= 200 ? circles.green : apiLatency <= 400 ? circles.yellow : circles.red} ${apiLatency}ms`,
-        //                 inline: true
-        //             },
-        //             {
-        //                 name: "Client Uptime",
-        //                 value: `${days}d ${hours}h ${minutes}m ${seconds}s`,
-        //                 inline: true
-        //             }
-        //         ])
-        //     ]
-        // });
+        msg.edit({ content: `The bots latency is: ${botLatency <= 200 ? circles.green : botLatency <= 400 ? circles.yellow : circles.red} ${botLatency}ms` }); 
     },
 };
